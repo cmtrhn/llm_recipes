111f2c55eb1f4125229499049e21addf9fdf2043
Cem TARHAN <112691888+cmtrhn@users.noreply.github.com>
2025-02-24 22:03:01 +0300

    Update train_a_translation_model.py

--- train_a_translation_model.py
+++ train_a_translation_model.py
@@ -271,5 +271,3 @@ if __name__ == "__main__":
         trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
     else:
         infer(args.checkpoint)
-
-    print(translate(model, dataset, "Eine weiße Katze rennt auf der Straße ."))
